a3c139eb63da3bce0a0949b3fe608dddafcd664d
shubhamyadav1996 <shubhamyadav@bitcot.com>
2026-04-21 12:21:45 +0530

    change in code

--- app/Http/Controllers/Auth2/AuthController.php
+++ app/Http/Controllers/Auth2/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
         ]);
           
         event(new Registered($user));
-        \Log::info('Event fired');
+        \Log::info('Event fired success fully');
 
         
         return true;
